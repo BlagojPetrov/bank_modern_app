3c0dfa9c8df705b4d36807f7a799fc483cb90243
Blagoj <petrov.blagoj@uklo.edu.mk>
2025-07-05 16:46:24 +0200

    feat: update Hero, Navbar, GetStarted components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testi
 
 const App = () => {
   return (
-    <div className='bg-blue-600 w-full overflow-hidden'>
+    <div className='bg-black w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
-    <div className={`bg-blue-700 ${styles.flexStart}`}>
+    <div className={`bg-black ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-blue-700 ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
