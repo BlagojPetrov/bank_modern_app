a5c6197ded3b1d773728f73b9c39fdee4b835264
Blagoj <petrov.blagoj@uklo.edu.mk>
2025-07-07 16:51:00 +0200

    Finish project: update CTA, Clients, Footer components and constants

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { airbnb, binance, coinbase, dropbox } from "../assets";
+import { clients } from "../constants";
+import styles, { layout } from "../style";
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className={layout.section}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] cursor-pointer object-contain transition duration-300 hover:brightness-200"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-export default Clients
+export default Clients;
